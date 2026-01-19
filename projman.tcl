161c946d29b5bce5deb7014ef12a8ac4ef9116eb
Vadim Ushakov <wandrien.dev@gmail.com>
2026-01-19 21:25:28 +0700

    Add proc stacktrace

--- projman.tcl
+++ projman.tcl
@@ -629,6 +629,23 @@ proc fireCallbacks {cbListRef args} {
     }
 }
 
+proc stacktrace {} {
+    set stack "Stack trace:\n"
+    for {set i 1} {$i < [info level]} {incr i} {
+        set lvl [info level -$i]
+        set pname [lindex $lvl 0]
+        append stack [string repeat " " $i]$pname
+        foreach value [lrange $lvl 1 end] arg [info args $pname] {
+            if {$value eq ""} {
+                info default $pname $arg value
+            }
+            append stack " $arg='$value'"
+        }
+        append stack "\n"
+    }
+    return $stack
+}
+
 ################################################################################
 
 # Extract version etc from the source code
